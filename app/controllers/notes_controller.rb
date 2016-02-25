d1823f87d3e8bed6f872f05054c21cb4263aeed7
Sumayyah Perine <sperine01@gmail.com>
2016-02-25 01:26:39 -0500

    edit notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -5,11 +5,16 @@ class NotesController < ApplicationController
 	end
 
 	def new
-		
+		@note = Note.new
 	end
 
 	def create
-		
+		@note = Note.new(note_params)
+		if @note.save
+			redirect_to @note
+		else
+			render "new"
+		end
 	end
 
 	def show
@@ -33,6 +38,6 @@ class NotesController < ApplicationController
 	end
 
 	def note_params
-		
+		params.require(:note).permit(:title, :content)
 	end
 end
